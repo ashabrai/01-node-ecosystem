effbb95bfc23661aca74f86e8f060892b267892c
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-09-11 18:19:09 -0700

    improper use for sub

--- __test__/arithmetic.test.js
+++ __test__/arithmetic.test.js
@@ -5,13 +5,13 @@ const arithmetic = require('../lib/arithmetic');
 
 describe('#arithmetic.test.js', () => {
   test('How to add things together', () => {
-    const addNum = arithmetic.addNumbers(A , 5);
+    const addNum = arithmetic.addNumbers(3, 5);
     expect(addNum).toEqual(8);
   });
 });
 describe('arithmetic.test.js', () => {
   test('How to subtract numbers', () => {
-    const subNum = arithmetic.subNumbers(5, 4);
+    const subNum = arithmetic.subNumbers(x, 4);
     expect(subNum).toEqual(1);
   });
 });
