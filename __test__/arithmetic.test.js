7ddd2d6402e2a9862a1d58bf59ed6ab08537a85e
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-09-11 18:11:24 -0700

    added test and function

---
+++ __test__/arithmetic.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const arithmetic = require('../lib/arithmetic');
+
+
+describe('#arithmetic.test.js', () => {
+  test('How to add things together', () => {
+    const addNum = arithmetic.addNumbers(3, 5);
+    expect(addNum).toEqual(8);
+  });
+});
+describe('arithmetic.test.js', () => {
+  test('How to subtract numbers', () => {
+    const subNum = arithmetic.subNumbers(5, 4);
+    expect(subNum).toEqual(1);
+  });
+});
